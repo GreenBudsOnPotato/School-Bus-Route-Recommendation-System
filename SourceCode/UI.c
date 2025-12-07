a77c51f8b5906e8d6405b21114e9b4492907bc74
GreenBudsOnPotato <3279197834@qq.com>
2025-12-07 16:39:37 +0800

    更新UI界面版本信息

--- SourceCode/UI.c
+++ SourceCode/UI.c
@@ -8,7 +8,7 @@ int UI(void)
 	int select = 0;
 	while (1)
 	{
-		printf("欢迎使用西南大学校车路线推荐系统！（Version-2025112601-Debug）\n");
+		printf("欢迎使用西南大学校车路线推荐系统！（Version-20251207-Debug）\n");
 		printf("************************************\n");
 		printf("* 1 校车路线表\t\t2 别名一览 *\n");
 		printf("* 3 校车乘坐推荐                   *\n");
@@ -18,7 +18,7 @@ int UI(void)
 		printf("请键入您的选择(1-5，输入0退出程序): ");
 		if (!(scanf("%d", &select)) || select < 0 || select >= 6)
 		{
-			fprintf(stderr,"输入有误,请重新输入！\n");
+			fprintf(stderr, "输入有误,请重新输入！\n");
 			clearerr(stdin);
 			system("pause");
 			system("cls");
